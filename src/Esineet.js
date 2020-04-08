1588168deef16ae78fa85e75105e6f267d0d3eed
Juuso Vuorinen <juvuorin@gmail.com>
2020-04-08 13:11:36 +0300

    useEffectin käyttö ja axios

---
+++ src/Esineet.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+
+//Axios, useEffect Context API, usereducer, REDUX
+const Esineet = (props) => {
+    const [data,setData] = useState(["Dataa haetaan"])
+    const [dataNoudettu, setDataNoudettu] = useState(false)
+    const [valittuPaikkakunta, setValittuPaikkakunta] = useState("")
+    useEffect(()=>{
+        async function haeDataa(){
+            let result = await axios('https://api.huuto.net/1.1/categories/'+props.id+'/items?area='+valittuPaikkakunta
+            )
+            //console.log(result)
+            let a = JSON.parse(result.request.response).items
+            setData(JSON.parse(result.request.response).items); 
+            setDataNoudettu(true);   
+        }
+        
+        haeDataa();
+
+    },[props.id, valittuPaikkakunta])
+    
+    const paikkakuntaTamperePainettu = ()=>{
+        setValittuPaikkakunta("Tampere")
+    }
+    const  paikkakuntaHelsinkiPainettu = ()=>{
+        setValittuPaikkakunta("Helsinki")
+    }
+    
+
+    return (<div>
+        <button onClick={paikkakuntaTamperePainettu}>Tampere</button>
+        <button onClick={paikkakuntaHelsinkiPainettu}>Helsinki</button>
+
+        {!dataNoudettu ? "Odotetaan dataa..." :  data.map(alkio=><div>{alkio.title +" Paikassa:"+alkio.location}</div>)}
+       
+        </div>)
+
+}
+
+export default Esineet
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App1 from './App1';
 import App2 from './App2';
 import App3 from './App3';
-//import App4 from './App4';
+import App4 from './App4';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App3/>, document.getElementById('root'));
+ReactDOM.render(<App4/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
